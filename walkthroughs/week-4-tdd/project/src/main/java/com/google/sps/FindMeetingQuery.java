8ccdaf8d9289dd2d961f8318ea9a88e73f4eade0
Deepti Vaidyanathan <deepti.vaidyanathan1@gmail.com>
2020-06-25 12:27:03 -0500

    Checks for valid duration and attendee values

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,45 @@
 
 package com.google.sps;
 
+
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    //Checks if we have a valid meeting duration and attendees
+    if (request.getDuration() > (24*60)) {
+        return Arrays.asList();
+    } else if(request.getAttendees().isEmpty()) {
+        return Arrays.asList(TimeRange.WHOLE_DAY);
+    } else {
+        // throw new UnsupportedOperationException("TODO: Implement this method.");
+        Collection<TimeRange> validTimes = new ArrayList<TimeRange>();
+        Collection<Event> attendeeEvents = getAttendeeEvents(events, request.getAttendees());
+        int start = TimeRange.START_OF_DAY;
+        return validTimes;
+        }
+
+    }
+  
+    private Collection<Event> getAttendeeEvents(Collection<Event> events, Collection<String> attendees) {
+        Collection<Event> aEvents = new HashSet<Event>();
+        for (Event e : events) {
+            //People in event e
+            Collection <String> ePeople = new ArrayList<String>(e.getAttendees());
+
+            //The attendees of our meeting that are in event e as well
+            ePeople.retainAll(attendees);
+
+            //If there is someone in event e who is also in our meeting, 
+            //add to event list to consider while scheduling our meeting
+            if(!ePeople.isEmpty()) {
+                aEvents.add(e);
+            }
+        }
+        return aEvents;
+    }
 }
