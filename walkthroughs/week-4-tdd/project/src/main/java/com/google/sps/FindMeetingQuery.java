74f1b0e365324c76f167046f6bdd4708f4da021e
Deepti Vaidyanathan <deepti.vaidyanathan1@gmail.com>
2020-07-07 00:50:43 -0400

    Adds descriptive comments and variable names for readability

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -22,19 +22,30 @@ import java.util.Collections;
 import java.util.HashMap;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    //Checks if we have a valid meeting duration and attendees
-    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
-        return Arrays.asList();
-    } else if(request.getAttendees().isEmpty()) {
-        return Arrays.asList(TimeRange.WHOLE_DAY);
-    } else {
+  
+  /**
+   * Finds possible time ranges for a specific meeting so that all participants may attend
+   * @param events All of the events for all possible people
+   * @param request The specific meeting request containing information about participants and duration
+   * @return A range of times for the meeting to occur
+   */
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        //Checks if we have a valid meeting duration and attendees
+        if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+            return Arrays.asList();
+        } 
+        if(request.getAttendees().isEmpty()) {
+            return Arrays.asList(TimeRange.WHOLE_DAY);
+        } 
+
+
         Collection<TimeRange> validTimes = new ArrayList<TimeRange>();
         Event[] attendeeEvents = getAttendeeEvents(events, request.getAttendees());
         
         int start = TimeRange.START_OF_DAY;
         int i = 0;
 
+        //Iterates through the meeting attendees' events to find open time ranges
         while (i < attendeeEvents.length) {
             //Create a new time range the start to the beginning of next event
             TimeRange valid = TimeRange.fromStartEnd(start, attendeeEvents[i].getWhen().start(), false);
@@ -54,11 +65,16 @@ public final class FindMeetingQuery {
         if (TimeRange.END_OF_DAY - start >= request.getDuration()) {
             validTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
         }
-        return validTimes;
-        }
+        return validTimes;    
 
     }
   
+    /**
+     * Finds and orders the events for certain people based on start time
+     * @param events All events for all possible people
+     * @param attendees The attendees that we are trying to find events for
+     * @return An array containing all of the attendees' events sorted in order of start time
+     */
     private Event[] getAttendeeEvents(Collection<Event> events, Collection<String> attendees) {
         //Hashmap with TimeRange keys allows us to sort the keys by start time and thereby sort the events
         HashMap<TimeRange, Event> aEvents = new HashMap<TimeRange, Event>();
@@ -76,6 +92,7 @@ public final class FindMeetingQuery {
             }
         }
         
+        //Sorts the events based on start time
         ArrayList<TimeRange> sortedRanges= new ArrayList<TimeRange>(aEvents.keySet());
         Collections.sort(sortedRanges, TimeRange.ORDER_BY_START);
         
